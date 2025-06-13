e48f75b07d6c8a458ec6d6f41e9d9657b8206c2a
sfoskitt <stephanie.foskitt@gmail.com>
2025-06-13 14:00:25 -0400

    move things around and add a couple tests to LC_11 - tool fighting day

--- leetcode/LC_11.java
+++ leetcode/LC_11.java
@@ -1,4 +1,4 @@
-class LC_11 {
+public class LC_11 {
     static int maxArea(int[] height) {
         int len = height.length;
         int max = 0;
@@ -20,13 +20,24 @@ class LC_11 {
         return max;
     }
 
+    public static void testMaxArea() {
+      int[] test1 = {1,1};
+      assert maxArea(test1) == 1;
+
+      int[] test2 = {1,8,6,2,5,4,8,3,7};
+      assert maxArea(test2) == 49;
+
+      System.out.println("tests ran");
+    }
+
     public static void main(String[] args) {
+      testMaxArea();
       int len = args.length;
       int[] height = new int[len];
       for (int i = 0; i < len; i++){
         height[i] = Integer.parseInt(args[i]);
       }
 
-      System.out.println(maxArea(height));
+      System.out.println("maxArea " + maxArea(height));
     }
 }
